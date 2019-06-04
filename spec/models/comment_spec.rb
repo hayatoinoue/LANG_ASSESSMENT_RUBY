9d854c9b084e24fb51894a4a2997be9f621be0f5
fenrir-ha-inoue <ha.inoue@fenrir.co.jp>
2019-06-04 17:29:04 +0900

    add: コメントモデルのバリデーションテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # 投稿者名、内容があれば有効な状態であること
   it "is valid with a contributor_name and content" do
     comment = Comment.new(
       contributor_name: "Tester Name",
@@ -10,7 +9,6 @@ RSpec.describe Comment, type: :model do
     expect(comment).to be_valid
   end
 
-  # 投稿者名がなければ無効な状態であること
   it "is invalid without a contributor_name" do
     comment = Comment.new(
       contributor_name: nil
@@ -19,12 +17,27 @@ RSpec.describe Comment, type: :model do
     expect(comment.errors[:contributor_name]).to include("can't be blank")
   end
 
-  # 内容がなければ無効な状態であること
+  it "is invalid with a contributer_name that has more than 30 characters" do
+    comment = Comment.new(
+      contributor_name: "1234567890123456789012345678901"
+    )
+    comment.valid?
+    expect(comment.errors[:contributor_name]).to include("is too long (maximum is 30 characters)")
+  end
+
   it "is invalid without content" do
     comment = Comment.new(
       content: nil
     )
     comment.valid?
     expect(comment.errors[:content]).to include("can't be blank")
+  end
+
+  it "is invalid with content that has more than 255 characters" do
+    comment = Comment.new(
+      content: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"
+    )
+    comment.valid?
+    expect(comment.errors[:content]).to include("is too long (maximum is 255 characters)")
   end
 end
