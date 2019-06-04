98753205fb4a1eb81cd55026892c8402cb782ca6
fenrir-ha-inoue <ha.inoue@fenrir.co.jp>
2019-06-04 18:02:14 +0900

    modify: factory-bot を用いて簡潔に

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,33 +6,33 @@ RSpec.describe Comment, type: :model do
   end
 
   it "is invalid without a contributor_name" do
-    comment = Comment.new(
-      contributor_name: nil
-    )
+    comment = build(:comment, contributor_name: nil)
     comment.valid?
     expect(comment.errors[:contributor_name]).to include("can't be blank")
   end
 
+  it "is valid with a contributer_name that has less than 31 characters" do
+    expect(build(:comment, contributor_name: "a"*30)).to be_valid
+  end
+
   it "is invalid with a contributer_name that has more than 30 characters" do
-    comment = Comment.new(
-      contributor_name: "1234567890123456789012345678901"
-    )
+    comment = build(:comment, contributor_name: "a"*31)
     comment.valid?
     expect(comment.errors[:contributor_name]).to include("is too long (maximum is 30 characters)")
   end
 
   it "is invalid without content" do
-    comment = Comment.new(
-      content: nil
-    )
+    comment = build(:comment, content: nil)
     comment.valid?
     expect(comment.errors[:content]).to include("can't be blank")
   end
 
+  it "is valid with content that has less than 256 characters" do
+    expect(build(:comment, content: "a"*255)).to be_valid
+  end
+
   it "is invalid with content that has more than 255 characters" do
-    comment = Comment.new(
-      content: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"
-    )
+    comment = build(:comment, content: "a"*256)
     comment.valid?
     expect(comment.errors[:content]).to include("is too long (maximum is 255 characters)")
   end
